7d71017167487122414c43895cc179914b39ae1e
aquilax <aquilax@gmail.com>
2016-12-11 19:02:24 +0100

    Bigger report

--- bin/report.php
+++ bin/report.php
@@ -10,13 +10,16 @@ if ($configFileName) {
 	$config = json_decode(file_get_contents($configFileName), true);
 }
 
+$config['#YESTERDAY'] = date('Y/m/d', strtotime('yesterday'));
+$config['#TODAY'] = date('Y/m/d');
+
 $commands = array(
     'command' => function($text, Mustache_LambdaHelper $helper) use ($config) {
         $text = trim($text);
         if ($text[0] === ';') {
             return $helper->render(' -- Commented -- ');
         }
-        $command = str_replace($config, array_values($config), $text);
+        $command = str_replace(array_keys($config), $config, $text);
         $output = array();
         exec($command, $output);
         return $helper->render(implode(PHP_EOL, $output));
